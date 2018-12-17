2d840130f2bb756a793da4564797a57267fce475
Shantanu <shanjohri@gmail.com>
2018-12-17 22:18:18 +0530

    stb

--- js/sketch.js
+++ js/sketch.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
-  const URL = "https://raw.githubusercontent.com/hanuor/spilltheblack/master/text.txt";
-  $.ajax({
-    url: URL,
-    type: "GET",
-    success: function(result) {
+//   const URL = "https://raw.githubusercontent.com/hanuor/spilltheblack/master/text.txt";
+//   $.ajax({
+//     url: URL,
+//     type: "GET",
+//     success: function(result) {
       
-var m = result.match(/<body[^>]*>([^<]*(?:(?!<\/?body)<[^<]*)*)<\/body\s*>/i);
-if (m){
- bodytext = m[1];
-  console.log (result)
-}
+// var m = result.match(/<body[^>]*>([^<]*(?:(?!<\/?body)<[^<]*)*)<\/body\s*>/i);
+// if (m){
+//  bodytext = m[1];
+//   console.log (result)
+// }
 
-    },
-    error: function(error) {
-      console.log('Error ${error}')
-    }
-  })
+//     },
+//     error: function(error) {
+//       console.log('Error ${error}')
+//     }
+//   })
 
 
 
