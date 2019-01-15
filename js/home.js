c8611f9b4cce2a1af6b4e26c42089292d32ba97c
Shantanu <shanjohri@gmail.com>
2019-01-15 23:08:52 +0530

    changes

--- js/home.js
+++ js/home.js
@@ -1,25 +1,26 @@
 $(document).ready(function () {
-    var masterString = readTextFile("https://raw.githubusercontent.com/hanuor/spilltheblack/master/uploader/data.html");
-    var head = masterString.match(/<head[^>]*>[\s\S]*<\/head>/gi);
-    var body = masterString.match(/<article[^>]*>[\s\S]*<\/article>/gi);
-    var style = masterString.match(/<style[^>]*>[\s\S]*<\/style>/gi);
-    $(style[0]).appendTo('body');
-    $(body[0]).appendTo('#showcase_content');
+
+    readTextFile("https://raw.githubusercontent.com/hanuor/spilltheblack/master/uploader/data.html");
+
 });
 
 function readTextFile(file) {
     var fetchedString = "";
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function () {
-        if (rawFile.readyState === 4) {
-            if (rawFile.status === 200 || rawFile.status == 0) {
-                fetchedString = rawFile.responseText;
-            }
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            var masterString = xhr.responseText;
+            var head = masterString.match(/<head[^>]*>[\s\S]*<\/head>/gi);
+            var body = masterString.match(/<article[^>]*>[\s\S]*<\/article>/gi);
+            var style = masterString.match(/<style[^>]*>[\s\S]*<\/style>/gi);
+            $(style[0]).appendTo('body');
+            $(body[0]).appendTo('#showcase_content');
+
         }
-    }
-    rawFile.send(null);
-    if (fetchedString.length != 0) {
-        return fetchedString;
-    }
+    };
+    xhr.open('GET', file, true);
+    xhr.send(null);
+    // if (fetchedString.length !== 0) {
+    //     return fetchedString;
+    // }
 }
