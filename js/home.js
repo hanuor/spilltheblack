6e9c917f4c9e86c49250d51dd91577b01a7f1816
Shantanu <shanjohri@gmail.com>
2019-01-15 23:28:42 +0530

    Cleaned

--- js/home.js
+++ js/home.js
@@ -5,22 +5,23 @@ $(document).ready(function () {
 });
 
 function readTextFile(file) {
-    var fetchedString = "";
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             var masterString = xhr.responseText;
             var head = masterString.match(/<head[^>]*>[\s\S]*<\/head>/gi);
             var body = masterString.match(/<article[^>]*>[\s\S]*<\/article>/gi);
+            var footer = body[0].match(/<footer[^>]*>[\s\S]*<\/footer>/gi);
+            var bodyPrint = body[0].replace(footer[0], '');
             var style = masterString.match(/<style[^>]*>[\s\S]*<\/style>/gi);
             $(style[0]).appendTo('body');
-            $(body[0]).appendTo('#showcase_content');
+            $(bodyPrint).appendTo('#showcase_content');
 
         }
     };
     xhr.open('GET', file, true);
     xhr.send(null);
-    // if (fetchedString.length !== 0) {
-    //     return fetchedString;
-    // }
 }
+
+
+
